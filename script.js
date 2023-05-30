4e7e1e75889843ae1e731447f55294de24f0522b
Mona Lisa <Sissy1310@aol.com>
2023-05-29 23:35:12 -0500

    fixed function

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ var temp = document.querySelector('.temp');
 button.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=a17fdc4c9b073b818693294055558b05')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather'][0]['description'];
+
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+})
 
 .catch(err => alert("Wrong city name!"))
 })
